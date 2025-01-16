61ea1c4a1ee530dd135fc6f0b16b0eee8f7c696a
Muna00 <68157263+Muna00@users.noreply.github.com>
2025-01-16 11:11:23 +0600

    others.py

--- models/others.py
+++ models/others.py
@@ -4,43 +4,38 @@ from models.login import Login
 
 
 
-# Base class for shared methods
+
 class Main(Login):
-    # def __init__(self, page: Page):
-    #     super().__init__(page)
-    # def close_all_popups(self):
-    #     self.close_popups()
-
-
+    
     def search_navigate(self, search_keyword: str):
-        # Click the search icon
+        
         search_icon = self.page.locator("svg[class='BingeSvgIcon-root BingeSvgIcon-fontSizeMedium css-4rspxq']")
         search_icon.click()
 
-        # Access the search bar
+       
         search_bar = self.page.get_by_placeholder("Search Shows and Movies...")
         expect(search_bar).to_be_visible()
         search_bar.click()
 
-        # Enter the search keyword
+        
         search_bar.fill(search_keyword)
         search_bar.press("Enter")
 
-        # Verify navigation to the search results page
+        
         search_result_page = "https://binge.buzz/search-result"
         expect(self.page).to_have_url(search_result_page)
         self.page.wait_for_timeout(1000)
 
 
-# Child class for premium search
+
 class Search_Keyword(Main):
     def premium_search_keyword(self):
-        # Return a predefined search keyword for premium content
+        
         return "Shodor Ghater Tiger S1 E5"
 
 
     def free_search_keyword(self):
-        # Return a predefined search keyword for premium content
+        
         return "Prohelika Trailer"
 
 
@@ -86,9 +81,7 @@ class Click_Pages(Main):
 
 class Player_Keys(Click_Pages):
         def __init__(self, page: Page):
-            # Ensure the parent class is initialized correctly
             super().__init__(page)
-            # Initialize play and pause buttons
             self.play_button = page.locator("svg[data-name='Play']")
             self.pause_button = page.locator('svg[data-name="Pause"]')
             self.backward_button=page.locator("svg[data-name='Back10']")
@@ -108,7 +101,7 @@ class Player_Keys(Click_Pages):
         
 
         def set_prerequisites(self):
-            # Perform login and navigation
+            
             self.login_navigate()
             self.navigate_to_trailer_section()
 
@@ -117,17 +110,15 @@ class Player_Keys(Click_Pages):
             expect(self.page.locator("div[class='BingeCardContent-root css-esr8dj']")).to_be_visible()
             self.page.wait_for_timeout(1000)
 
-            # Click the play button
+            # Click play button
             self.page.locator("button[class='BingeBtnBase-root css-1lo0mph']").click()
             self.page.wait_for_timeout(1000)
             self.page.mouse.move(100, 100)
 
-        def click_Play(self):
-            # Click the Play button
+        def click_Play(self):    
             self.play_button.click()
 
         def click_Pause(self):
-            # Click the Pause button
             self.pause_button.click()
 
         def click_Backward(self):
@@ -164,8 +155,7 @@ class Player_Keys(Click_Pages):
         def hoverOverCircle(self):
             self.cursorCircle.hover()
 
-
-
+    
 
 
         def get_cursorTimeStamp(self):
